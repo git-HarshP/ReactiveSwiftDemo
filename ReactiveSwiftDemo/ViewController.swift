9b54e6f2fb977f717422f43ac7b9290adbd27583
HarshPipaliya <harsh.pipaliya@difx.io>
2023-04-08 17:57:13 +0400

    Added demo of RXSwift with TableView

--- ReactiveSwiftDemo/ViewController.swift
+++ ReactiveSwiftDemo/ViewController.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+
+struct Product {
+    let imageName: String
+    let title: String
+}
+
+
+struct ProductViewModel {
+    var item = PublishSubject<[Product]>()
+     
+    func fetchItems() {
+        let products = [
+            Product(imageName: "house", title: "Home"),
+            Product(imageName: "gear", title: "Settings"),
+            Product(imageName: "person.circle", title: "Profile"),
+            Product(imageName: "airplane", title: "Flight"),
+            Product(imageName: "bell", title: "Activity"),
+        ]
+        
+        item.onNext(products)
+        item.onCompleted()
+    }
+}
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet var productsTableView: UITableView!
+    
+    private var viewModel = ProductViewModel()
+    private var bag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        productsTableView.register(UITableViewCell.self ,forCellReuseIdentifier: "cell")
+        bindTableData()
     }
 
+    
+    func bindTableData() {
+        viewModel.item.bind(
+            to: productsTableView.rx.items(
+                cellIdentifier: "cell",
+                cellType: UITableViewCell.self)) { row, model, cell in
+                    cell.textLabel?.text = model.title
+                    cell.imageView?.image = UIImage(systemName: model.imageName)
+                }.disposed(by: bag)
+
+         _ = productsTableView.rx.modelSelected(Product.self).bind { product in
+            print("### \(product.title)")
+        }
+        
+        viewModel.fetchItems()
+    }
 
 }
 
